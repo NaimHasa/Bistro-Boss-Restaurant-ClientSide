25cecadebdff6c52e7d79112ebdd5fe62fb7b4c0
Naim Hasan <naimarafat7@gmail.com>
2023-07-31 04:03:55 -0700

    Top Banner react responsive carousel

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -10,17 +10,17 @@ const Navbar = () => {
     ];
 
     return (
-        <div className="navbar bg-gray-300">
+        <div className="navbar fixed z-10 bg-opacity-50 bg-slate-400 max-w-7xl mx-auto">
             <div className="navbar-start ">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm font-semibold uppercase dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sms font-bold uppercase dropdown-content mt-3 z-[1] p-2  shado rounded-box w-52">
                         {navItems}
                     </ul>
                 </div>
-                <a className="text-teal-700 font-bold uppercase">
+                <a className="text-white font-bold uppercase">
                     Bistro Boss
                     <br />
                     restaurant
@@ -28,7 +28,7 @@ const Navbar = () => {
                 </a>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 uppercase font-semibold">
+                <ul className="menu menu-horizontal  px-1 uppercase font-bold">
                     {navItems}
                 </ul>
             </div>
