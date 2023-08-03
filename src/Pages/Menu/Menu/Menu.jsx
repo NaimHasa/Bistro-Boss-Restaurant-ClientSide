fff26775cdfeb632ba885ede851979c1cb2daa92
Naim Hasan <naimarafat7@gmail.com>
2023-08-03 04:03:27 -0700

    Cover With Blur Parallax Using React Parallax

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,22 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss || Menu</title>
             </Helmet>
-            <h1>Men men menu</h1>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
---
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -0,0 +1,17 @@
+
+const Cover = ({ img, title }) => {
+    return (
+        <div className="hero h-[600px]" style={{ backgroundImage: `url('${img}')` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-outline text-white border-0 border-b-4 uppercase text-2xl mt-4">Order Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cover;
