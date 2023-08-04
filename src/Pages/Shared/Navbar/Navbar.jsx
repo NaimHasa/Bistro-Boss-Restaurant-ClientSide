b8bcffb351a463e9b9e8d12e02bb4b2eb76e340d
Naim Hasan <naimarafat7@gmail.com>
2023-08-04 16:12:33 -0700

    Simple Food Order page with tabs

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
         <>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/menu'>Our Menu</Link></li>
+            <li><Link to='/order'>Order</Link></li>
             <li><a>Contact Us</a></li>
             <li><a>DeshBoard</a></li>
             <li><a>Our Shop</a></li>
--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -4,6 +4,7 @@ import {
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu/Menu";
+import Order from "../Pages/Order/Order";
 
 
 export const router = createBrowserRouter([
@@ -20,6 +21,11 @@ export const router = createBrowserRouter([
                 path: '/menu',
                 element: <Menu></Menu>
             },
+            {
+                path: '/order',
+                element: <Order></Order>
+
+            }
 
         ]
     },
