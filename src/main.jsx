9560e1956c0c4b8c4fa635e2a3a7a10ca787e477
Naim Hasan <naimarafat7@gmail.com>
2023-08-09 12:48:01 -0700

    React Query and load data

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,23 @@ import {
 import { router } from './Router/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Provider/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+const queryClient = new QueryClient()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <div className='max-w-7xl mx-auto bg-slate-200'>
-          <RouterProvider router={router} />
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <div className='max-w-7xl mx-auto bg-slate-200'>
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
+
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>,
