dc491783b1fee3ee43254ad62409072fc1cd2436
Naim Hasan <naimarafat7@gmail.com>
2023-08-09 02:32:06 -0700

    API naming convention and API to save Cart

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,8 +1,55 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
+
 
 const FoodCard = ({ item }) => {
-    const { image, price, name, recipe } = item;
+    const { name, image, price, recipe, _id } = item;
+    const { user } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
     const handleAddToCart = item => {
-        console.log(item)
+        console.log(item);
+        if (user && user.email) {
+            const cartItem = { menuItemId: _id, name, image, price, email: user.email }
+            fetch('http://localhost:5000/carts', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(cartItem)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        // refetch(); // refetch cart to update the number of items in the cart
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Food added on the cart.',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: 'Please login to order the food',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } })
+                }
+            })
+        }
     }
     return (
         <div className="card w-96 bg-base-100 shadow-xl mt-5 p-4">
