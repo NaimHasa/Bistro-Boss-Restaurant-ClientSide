a6a8947ea6abc9023e52dafaad043aeab9f1e7a2
Naim Hasan <naimarafat7@gmail.com>
2023-08-10 06:05:56 -0700

    Display cart items to review before order

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -15,16 +15,16 @@ const Dashboard = () => {
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content uppercase">
                     {/* Sidebar content here */}
-                    <li><Link><FcHome></FcHome> User Home </Link></li>
-                    <li><Link><FcCalendar></FcCalendar> Reservation </Link></li>
-                    <li><Link><FcMoneyTransfer></FcMoneyTransfer> Payment History </Link></li>
+                    <li><Link to='/deshboard/home'><FcHome></FcHome> User Home </Link></li>
+                    <li><Link to='/deshboard/reservation'><FcCalendar></FcCalendar> Reservation </Link></li>
+                    <li><Link to='/deshboard/paymenthistory'><FcMoneyTransfer></FcMoneyTransfer> Payment History </Link></li>
                     <li><Link to='/deshboard/mycart'><FcPodiumWithAudience></FcPodiumWithAudience> My Cart </Link></li>
 
                     <div className="divider"></div>
                     <li><Link to='/'> <FcHome></FcHome> Home </Link></li>
-                    <li><Link to='/'> <FcMenu></FcMenu> Menu </Link></li>
-                    <li><Link><FcShop></FcShop> Shop</Link></li>
-                    <li><Link to=''><FcContacts></FcContacts> Contact</Link></li>
+                    <li><Link to='/menu'> <FcMenu></FcMenu> Menu </Link></li>
+                    <li><Link to='/deshboard/shop'><FcShop></FcShop> Shop</Link></li>
+                    <li><Link to='/deshboard/contact'><FcContacts></FcContacts> Contact</Link></li>
 
                 </ul>
 
--- src/Pages/Deshboard/MyCart/MyCart.jsx
+++ src/Pages/Deshboard/MyCart/MyCart.jsx
@@ -1,8 +1,63 @@
+import { Helmet } from "react-helmet-async";
+import useCarts from './../../../hooks/useCarts';
+import { FcDeleteRow } from "react-icons/fc";
 
 const MyCart = () => {
+    const [cart] = useCarts();
+    const total = cart.reduce((sum, item) => item.price + sum, 0)
     return (
         <div>
-            <h2>My Add to Cart</h2>
+            <Helmet>
+                <title>Bistro Boss || My Cart</title>
+            </Helmet>
+            <div className="uppercase text-xl text-white font-bold flex justify-evenly items-center h-[60px] bg-gray-800 rounded-md">
+                <h2>Total Orders: {cart.length}</h2>
+                <h2>Total Price: ${total}</h2>
+                <button className="btn btn-outline btn-warning btn-sm">Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>ITEM IMAGE</th>
+                            <th>ITEM NAME</th>
+                            <th>PRICE</th>
+                            <th>ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span className="badge badge-ghost badge-sm">{item.name}</span>
+                                </td>
+                                <td>{item.price}</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-sm text-red-500"><FcDeleteRow></FcDeleteRow> Deleted</button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
