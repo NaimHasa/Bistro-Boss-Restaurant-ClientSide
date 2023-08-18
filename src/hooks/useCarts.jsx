4e87f1668fc246dceb69d202eae6d020c3decd37
Naim Hasan <naimarafat7@gmail.com>
2023-08-18 15:07:36 -0700

    Send JWT token to back end for verification

--- src/hooks/useCarts.jsx
+++ src/hooks/useCarts.jsx
@@ -5,10 +5,16 @@ import { AuthContext } from "../Provider/AuthProvider";
 
 const useCarts = () => {
     const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
+
     const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            })
             return res.json();
         },
     })
