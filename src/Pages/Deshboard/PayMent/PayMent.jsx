be255f4f5e92ad98bc38d6af6e221227d4560a83
Naim Hasan <naimarafat7@gmail.com>
2023-08-28 13:42:33 -0700

    Get Price for payment intent and understand array reduce

--- src/Pages/Deshboard/PayMent/PayMent.jsx
+++ src/Pages/Deshboard/PayMent/PayMent.jsx
@@ -3,15 +3,23 @@ import { Elements } from '@stripe/react-stripe-js';
 import SectionTitle from './../../../Components/SectionTittle/SectionTitle';
 import CheckoutForm from './CheckoutForm';
 import { loadStripe } from '@stripe/stripe-js';
-const PayMent = () => {
-    //TODO: Pk Key Peyment Method 
+import useCarts from '../../../hooks/useCarts';
 
-    const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+//TODO: Pk Key Peyment Method 
+
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+
+const PayMent = () => {
+    const [cart] = useCarts();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2))
+
+
     return (
         <div className='w-full'>
             <SectionTitle heading="Please Provide Your" subHeading="Payment Info" />
             <Elements stripe={stripePromise}>
-                <CheckoutForm>
+                <CheckoutForm price={price}>
 
                 </CheckoutForm>
             </Elements>
